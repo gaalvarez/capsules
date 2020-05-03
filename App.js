ec9157fb2c34da44fb516bafce79e2b3f66fdf31
Gustavo Álvarez <gustavo.alvarez@iotecsoluciones.co>
2020-05-03 15:49:45 -0500

    Correr directamente en un telefono android

--- App.js
+++ App.js
@@ -5,9 +5,6 @@ import Detalle from "./vistas/Detalle";
 import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 
-import { AppLoading } from "expo";
-import * as Font from "expo-font";
-
 import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
@@ -24,32 +21,7 @@ function CapsulasStack() {
 
 // Componente con la sintaxis de clase
 export default class App extends Component {
-  state = {
-    fuentesCargadas: false,
-  };
-
-  async componentDidMount() {
-    await Font.loadAsync({
-      "Rubik-Black": require("./node_modules/@shoutem/ui/fonts/Rubik-Black.ttf"),
-      "Rubik-BlackItalic": require("./node_modules/@shoutem/ui/fonts/Rubik-BlackItalic.ttf"),
-      "Rubik-Bold": require("./node_modules/@shoutem/ui/fonts/Rubik-Bold.ttf"),
-      "Rubik-BoldItalic": require("./node_modules/@shoutem/ui/fonts/Rubik-BoldItalic.ttf"),
-      "Rubik-Italic": require("./node_modules/@shoutem/ui/fonts/Rubik-Italic.ttf"),
-      "Rubik-Light": require("./node_modules/@shoutem/ui/fonts/Rubik-Light.ttf"),
-      "Rubik-LightItalic": require("./node_modules/@shoutem/ui/fonts/Rubik-LightItalic.ttf"),
-      "Rubik-Medium": require("./node_modules/@shoutem/ui/fonts/Rubik-Medium.ttf"),
-      "Rubik-MediumItalic": require("./node_modules/@shoutem/ui/fonts/Rubik-MediumItalic.ttf"),
-      "Rubik-Regular": require("./node_modules/@shoutem/ui/fonts/Rubik-Regular.ttf"),
-      "rubicon-icon-font": require("./node_modules/@shoutem/ui/fonts/rubicon-icon-font.ttf"),
-    });
-
-    this.setState({ fuentesCargadas: true });
-  }
-
   render() {
-    if (!this.state.fuentesCargadas) {
-      return <AppLoading />;
-    }
     return (
       <NavigationContainer>
         <CapsulasStack />
